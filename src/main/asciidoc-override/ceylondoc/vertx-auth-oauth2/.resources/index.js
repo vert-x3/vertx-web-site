ead453809d687f4f3ec92cb004f4472ae916fd3b
Clement Escoffier <clement.escoffier@gmail.com>
2016-03-15 17:20:53 +0100

    Publish the ceylon documentation for 3.2.2 Publish the ceylondoc documentation for 3.2.2

---
+++ src/main/asciidoc-override/ceylondoc/vertx-auth-oauth2/.resources/index.js
@@ -0,0 +1,18 @@
+var index = [
+{'name': 'io.vertx.ceylon.auth.oauth2', 'type': 'package', 'url': 'index.html#section-package', 'doc': '', 'tags': [],'icons': ['icon-package']},
+{'name': 'OAuth2Auth.api', 'type': 'function', 'url': 'OAuth2Auth.type.html#api', 'doc': '<p>Call OAuth2 APIs.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'OAuth2Auth.getToken', 'type': 'function', 'url': 'OAuth2Auth.type.html#getToken', 'doc': '<p>Returns the Access Token object.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'OAuth2Auth.authorizeURL', 'type': 'function', 'url': 'OAuth2Auth.type.html#authorizeURL', 'doc': '<p>Generate a redirect URL to the authN/Z backend.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'OAuth2Auth', 'type': 'class', 'url': 'OAuth2Auth.type.html', 'doc': '<p>Factory interface for creating OAuth2 based <code>io.vertx.ext.auth.AuthProvider</code> instances.</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'oAuth2Auth.create', 'type': 'function', 'url': 'oAuth2Auth.object.html#create', 'doc': '<p>Create a OAuth2 auth provider</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'oAuth2Auth.create', 'type': 'function', 'url': 'oAuth2Auth.object.html#create', 'doc': '<p>Create a OAuth2 auth provider</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'oAuth2Auth.create', 'type': 'function', 'url': 'oAuth2Auth.object.html#create', 'doc': '<p>Create a OAuth2 auth provider</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'oAuth2Auth', 'type': 'object', 'url': 'oAuth2Auth.object.html', 'doc': '', 'tags': [],'icons': ['icon-object']},
+{'name': 'oAuth2Auth', 'type': 'value', 'url': 'index.html#oAuth2Auth', 'doc': '', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'AccessToken.refresh', 'type': 'function', 'url': 'AccessToken.type.html#refresh', 'doc': '<p>Refresh the access token</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'AccessToken.expired', 'type': 'function', 'url': 'AccessToken.type.html#expired', 'doc': '<p>Check if the access token is expired or not.</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'AccessToken.revoke', 'type': 'function', 'url': 'AccessToken.type.html#revoke', 'doc': '<p>Revoke access or refresh token</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'AccessToken', 'type': 'class', 'url': 'AccessToken.type.html', 'doc': '<p>AccessToken extension to the User interface</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'io.vertx.ceylon.auth.oauth2', 'type': 'module', 'url': 'index.html', 'doc': '', 'tags': [],'icons': ['icon-module']}];
+var tagIndex = [
+];
