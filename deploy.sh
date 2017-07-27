a4ce9fa88011fd7e1e3f09d54d61ccd42acdae02
Julien Ponge <julien.ponge@gmail.com>
2017-07-27 15:59:26 +0200

    Fix deploy.sh to use HTTPS rather than Git+SSH

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "Deploying web site"
 cd target
 rm -Rf scmpublish-checkout
-git clone --branch master git@github.com:vert-x3/vert-x3.github.io.git scmpublish-checkout
+git clone --branch master https://github.com/vert-x3/vert-x3.github.io.git scmpublish-checkout
 echo "Updating checkout directory with actual content in target/site"
 cp -R site/* scmpublish-checkout/
 cd scmpublish-checkout
