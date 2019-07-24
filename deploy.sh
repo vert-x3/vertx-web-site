fcea35c74420d71d2138f67f37559e7ee4041962
Julien Viet <julien@julienviet.com>
2019-07-24 15:14:41 +0200

    deploy to Vertx 4 preview

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo "Deploying web site"
 cd target  || exit -1
 rm -Rf scmpublish-checkout
-git clone --depth=1 --branch master git@github.com:vert-x3/vert-x3.github.io.git scmpublish-checkout
+git clone --depth=1 --branch master git@github.com:vertx-ci/vertx-4-preview.git scmpublish-checkout
 echo "Updating checkout directory with actual content in target/site"
 cp -R site/* scmpublish-checkout/
 cd scmpublish-checkout  || exit -1
